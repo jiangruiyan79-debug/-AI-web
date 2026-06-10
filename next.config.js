d0e5c85663b4231ca66ebf654d50db3e084471f1
Yyan <jiangruiyan@YyanMacBook.local>
2026-06-10 22:39:41 +0800

    fix: 适配Next.js新版静态导出

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: ''
+}
+
+module.exports = nextConfig
